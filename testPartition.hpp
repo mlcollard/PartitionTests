44a38dc779918b198af2686c272871052c065dff
Michael L. Collard <collard@uakron.edu>
2021-04-12 15:49:53 -0400

    Refactor templates

--- testPartition.hpp
+++ testPartition.hpp
@@ -8,90 +8,64 @@
 #include <cassert>
 #include <bitset>
 
-template <typename Partition>
-void testHoarePartition(Partition partition) {
+// Tests a sort
+template <typename Sort>
+void testSort(Sort sort) {
 
-    std::function<void(std::vector<int>& a, int low, int high)> sortinner;
-    sortinner = [&partition,&sortinner](std::vector<int>& a, int low, int high) {
-
-        if (low >= high)
-            return;
-
-        auto p = partition(a, low, high);
-        sortinner(a, low, p);
-        sortinner(a, p + 1, high);
-    };
-
-    // convenience form for tests
-    auto sort = [&sortinner](std::vector<int>& a) {
-        sortinner(a, 0, a.size() - 1);
-    };
-
-    constexpr int SIZE = 8;
+    constexpr int SIZE = 10;
     for (int i = 0; i < std::pow(2, SIZE); ++i) {
-        std::bitset<SIZE> bs = i;
 
+        // generate bit string in vector
         std::vector<int> num;
+        std::bitset<SIZE> bs = i;
         for (auto c : bs.to_string())
             num.push_back(c - '0');
         std::reverse(num.begin(), num.end());
 
-        sort(num);
+        // perform the sort on this value in sequenc
+        sort(num, 0, num.size() - 1);
 
-        // output error
-        if (!std::is_sorted(num.begin(), num.end())) {
-            std::cerr << "Partition error: ";
-            for (int i = 0; i < SIZE; ++i)
-                std::cerr << num[i];
-            std::cerr << '\n';
-        }
+        // output and if no error overwrite line on next output
+        std::cout << '\r';
+        std::copy(num.begin(), num.end(), std::ostream_iterator<int>(std::cout, ""));
         assert(std::is_sorted(num.begin(), num.end()));
     }
 }
 
+// Tests a Lomuto Partitition in a Quicksort
 template <typename Partition>
 void testLomutoPartition(Partition partition) {
 
-    std::function<void(std::vector<int>& a, int low, int high)> sortinner;
-    sortinner = [&partition,&sortinner](std::vector<int>& a, int low, int high) {
+    std::function<void(std::vector<int>& a, int low, int high)> sort;
+    sort = [&partition,&sort](auto& a, int low, int high) {
 
         if (low >= high)
             return;
 
         auto p = partition(a, low, high);
-        sortinner(a, low, p - 1);
-        sortinner(a, p + 1, high);
+        sort(a, low, p - 1);
+        sort(a, p + 1, high);
     };
 
-    // convenience form for tests
-    auto sort = [&sortinner](std::vector<int>& a) {
-        sortinner(a, 0, a.size() - 1);
+    // test the sort
+    testSort(sort);
+}
+
+// Tests a Hoare Partitition in a Quicksort
+template <typename Partition>
+void testHoarePartition(Partition partition) {
+
+    std::function<void(std::vector<int>& a, int low, int high)> sort;
+    sort = [&partition,&sort](auto& a, int low, int high) {
+
+        if (low >= high)
+            return;
+
+        auto p = partition(a, low, high);
+        sort(a, low, p);
+        sort(a, p + 1, high);
     };
 
-    const int SIZE = 10;
-    for (int i = 0; i < std::pow(2, SIZE); ++i) {
-        int n = i;
-        std::vector<int> num;
-        while (n) {
-            if (n % 2) {
-                num.push_back(1);
-            }
-            else {
-                num.push_back(0);
-            }
-            n = n / 2;
-        }
-        for (int i = num.size(); i < SIZE; ++i)
-            num.push_back(0);
-
-        std::reverse(num.begin(), num.end());
-        sort(num);
-        if (!std::is_sorted(num.begin(), num.end())) {
-            std::cerr << "Partition error: ";
-            for (int i = 0; i < SIZE; ++i)
-                std::cerr << num[i];
-            std::cerr << '\n';
-        }
-        assert(std::is_sorted(num.begin(), num.end()));
-    }
+    // test the sort
+    testSort(sort);
 }
