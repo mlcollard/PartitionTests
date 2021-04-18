aadb5e0cf91abb8bdfef2f8306c45746f132908c
Michael L. Collard <collard@uakron.edu>
2021-04-18 14:53:22 -0400

    Add another Hoare implementation

--- PartitionHoare.cpp
+++ PartitionHoare.cpp
@@ -43,6 +43,36 @@ int main() {
         }
     });
 
+    // Hoare Partition Try with while https://bit.ly/3mYWHsZ
+    testHoarePartition([](std::vector<int>& a, int left, int right)->int {
+
+        int pivot = a[std::floor((left + right) / 2)];
+        while (left <= right) {
+
+            // move left to the right until value >= pivot
+            // which means the value at left must be swapped
+            while (a[left] < pivot)
+                ++left;
+
+            // move right to the left until value <= pivot
+            // which means the value at right must be swapped
+            while (a[right] > pivot)
+                --right;
+
+            // when crossed over, value from [low, right) are all < pivot
+            // and values from [right, high]
+            if (left <= right) {
+                // swap values at left & right since a[left] >= pivot and a[right] is <= pivot
+                std::swap(a[left], a[right]);
+                ++left;
+                --right;
+            }
+        }
+
+        // Note: the implementation is off-by-one from our quicksort for Hoare
+        return left - 1;
+    });
+
     // Hoare Partition Wikipedia Algorithm While Adjusted
     testHoarePartition([](std::vector<int>& a, int low, int high)->int {
 
